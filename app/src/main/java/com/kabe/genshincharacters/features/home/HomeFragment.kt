710eb4b6f2e9e37a754bd3dadc16f2e9223f03b9
Aldrin Mayor <kabecoder@gmail.com>
2024-04-23 01:45:59 +0800

    feat(home): display list of characters name

--- app/src/main/java/com/kabe/genshincharacters/features/home/HomeFragment.kt
+++ app/src/main/java/com/kabe/genshincharacters/features/home/HomeFragment.kt
@@ -1,12 +1,18 @@
 package com.kabe.genshincharacters.features.home
 
+import androidx.fragment.app.viewModels
 import com.kabe.genshincharacters.databinding.FragmentHomeBinding
+import com.kabe.genshincharacters.domain.Characters
 import com.kabe.genshincharacters.features.base.BaseFragment
 import com.kabe.genshincharacters.util.extensions.goTo
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(
     FragmentHomeBinding::inflate
 ) {
+    private val viewmodel: HomeFragmentViewModel by viewModels()
 
     override fun setUpView() {
         super.setUpView()
@@ -15,10 +21,23 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
             goTo(HomeFragmentDirections.actionHomeFragmentToDetailsFragment())
         }
 
+        viewmodel.getCharacters(true)
+
+
     }
 
     override fun collectViewModel() {
         super.collectViewModel()
+
+        collect(viewmodel.characters) { characters ->
+            updateTextView(characters)
+        }
+    }
+
+
+    private fun updateTextView(characters: List<Characters>) {
+        val text = characters.joinToString(separator = "\n") { it.name }
+        viewBinding.textView.text = text
     }
 
     override fun loadContent() {
---
+++ app/src/main/java/com/kabe/genshincharacters/features/home/HomeFragmentViewModel.kt
@@ -0,0 +1,72 @@
+package com.kabe.genshincharacters.features.home
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kabe.genshincharacters.constant.PaginationLoadingIndicator
+import com.kabe.genshincharacters.data.base.Status
+import com.kabe.genshincharacters.domain.Characters
+import com.kabe.genshincharacters.repository.characters.CharactersRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class HomeFragmentViewModel @Inject constructor(
+    private val charactersRepository: CharactersRepository
+): ViewModel() {
+
+    private val _loadingState = MutableStateFlow<PaginationLoadingIndicator>(PaginationLoadingIndicator.NONE)
+    val loadingState: StateFlow<PaginationLoadingIndicator> = _loadingState.asStateFlow()
+
+    private val _errorMessage = MutableSharedFlow<String?>()
+    val errorMessage: SharedFlow<String?> = _errorMessage.asSharedFlow()
+
+    private val charactersList = mutableListOf<Characters>()
+    private val _characters = MutableSharedFlow<MutableList<Characters>>()
+    val characters: SharedFlow<List<Characters>> = _characters.asSharedFlow()
+
+    private var currentPage = 0
+
+    fun getCharacters(resetPage: Boolean = false) {
+
+        viewModelScope.launch {
+
+            if (resetPage) {
+                currentPage = 0
+            }
+
+            val result = charactersRepository.getCharacters(currentPage + 1)
+
+            when (result.status) {
+                Status.SUCCESS -> result.data?.let {
+
+                    if (resetPage) {
+                        charactersList.clear()
+                    }
+
+                    charactersList.addAll(it)
+                    currentPage += 1
+                    _characters.emit(charactersList)
+
+                    Log.d("HomeFragmentViewmodel", "Success")
+
+                }
+                Status.ERROR -> {
+                    result.message?.let { _errorMessage.emit(it) }
+                    Log.d("HomeFragmentViewmodel", "Failed")
+                }
+            }
+
+            _loadingState.value = PaginationLoadingIndicator.NONE
+
+        }
+
+    }
+}
